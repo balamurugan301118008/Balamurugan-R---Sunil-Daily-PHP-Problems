9fa9d175557432ecad93a63a6be68c848501e94e
balamurugan301118008 <balamuruganrdckap@gmail.com>
2023-05-29 12:57:42 +0530

    completed

--- Palindrome.php
+++ Palindrome.php
@@ -1,3 +1,28 @@
 <?php
-    echo "helll world";
+
+// // below the code for only the checking conditions 
+    function Palindrome($input){
+        $rev = strrev($input);
+    //  echo $rev;
+      if($input === $rev){
+            echo $input." "."is  a palindrome"."\n";
+        }
+        else{
+            echo $input." "."not a palindrome"."\n";
+        }
+    }
+    Palindrome('radar');
+
+echo "*****************************"."\n";
+//Below is the code to convert the palindrome from the input value
+
+$value = readline("Enter a word: ");
+
+if($value === strrev($value)){
+    echo $value." "."is a palindrome word"."\n";
+}
+else{   
+    echo $value." "."is not a palindrome word"."\n";
+}
+
 ?>  

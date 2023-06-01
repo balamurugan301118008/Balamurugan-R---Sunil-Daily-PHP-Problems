f6a7aee4eb35f6ab230e863750daf714c2d87e12
balamurugan301118008 <balamuruganrdckap@gmail.com>
2023-06-01 14:53:28 +0530

    completed

---
+++ sortArray.php
@@ -0,0 +1,13 @@
+<?php
+// below the code to  ascending order an array
+$ascending = [23, 8, 6, 2, 3, 9, 4, 7, 43, 32, 5, 76, 1, 23];
+asort($ascending);
+print_r($ascending);
+
+echo "***********************";
+
+// below the code to  ascending order an array
+$decending = [3, 4, 5, 56, 34, 2, 32, 34, 6, 43, 32, 1];
+rsort($decending);
+print_r($decending);
+?>

c9a1a1f48161ccde046b79393db883640b14dd1b
balamurugan301118008 <balamuruganrdckap@gmail.com>
2023-06-01 11:12:44 +0530

    completed

--- leapYear.php
+++ leapYear.php
@@ -1,9 +1,10 @@
 <?php
 
-$year = readline("Enter a year: ");
-if ($year % 4 == 0) {
-    echo $year . " is a leap year" . "\n";
-} else {
-    echo $year . " is not a leap year" . "\n";
-}
+    $year = readline("Enter a year: ");
+    if ($year % 4 == 0) {
+        echo $year . " is a leap year" . "\n";
+    } else {
+        echo $year . " is not a leap year" . "\n";
+    }
+
 ?>

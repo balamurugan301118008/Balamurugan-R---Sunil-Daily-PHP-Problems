aeaded7cc4c49c59a0f4df7aecbae4e98425bf63
balamurugan301118008 <balamuruganrdckap@gmail.com>
2023-06-06 11:28:43 +0530

    problem solved

--- checkIfArrayEmptyOrNot.php
+++ checkIfArrayEmptyOrNot.php
@@ -1,4 +1,11 @@
 <?php
+// echo "check if array empty or not"."\n";
 
-
-echo "check if array empty or not"."\n";
+$array = [];
+if ($array === []) {
+    echo "Array is empty" . "\n";
+    print_r($array);
+} else {
+    echo "Array is not empty" . "\n";
+    print_r($array);
+}
